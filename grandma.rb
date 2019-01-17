8cbcba340398d23158bb8efbf7cb72857606eaaf
Alyssa Brown <alyssa.brown@live.com>
2019-01-17 22:08:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
    if phrase.upcase == phrase.upcase 
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  elsif phrase == phrase.upcase 
     puts "NO, NOT SINCE 1938!"
   else  phrase == phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
