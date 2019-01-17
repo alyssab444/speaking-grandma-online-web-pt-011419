ebacba005b14f4e84834ec6017729149b19c2688
Alyssa Brown <alyssa.brown@live.com>
2019-01-17 22:33:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(string)
     if string != string.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.upcase 
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else string == string
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
 end 
 end 
 
